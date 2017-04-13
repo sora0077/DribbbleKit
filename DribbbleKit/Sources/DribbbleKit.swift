b50aea7126acb0ac48825e44a550daa6f1ad684d
t_hayashi <t.hayashi0077+github@gmail.com>
2017-04-14 01:29:15 +0900

    Setup environment

---
+++ DribbbleKit/Sources/DribbbleKit.swift
@@ -0,0 +1,14 @@
+//
+//  DribbbleKit.swift
+//  DribbbleKit
+//
+//  Created by 林達也 on 2017/04/14.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+import Foundation
+import APIKit
+
+public protocol Request: APIKit.Request {
+
+}
