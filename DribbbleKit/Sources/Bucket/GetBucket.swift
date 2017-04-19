d6ef5fe355221d2f38ce8fa862ca231137a10020
sora0077 <t.hayashi0077+github@gmail.com>
2017-04-19 15:09:27 +0900

    Add GetUserRequest

--- DribbbleKit/Sources/Bucket/GetBucket.swift
+++ DribbbleKit/Sources/Bucket/GetBucket.swift
@@ -26,29 +26,13 @@ public struct GetBucket<Data: GetBucketData>: GetRequest {
 }
 
 public protocol GetBucketData: Decodable {
+    associatedtype User: GetUserData
     init(
         id: Bucket.Identifier,
         name: String,
         description: String,
         shotsCount: Int,
-        bucketsCount: Int,
-        commentsRecievedCount: Int,
-        followersCount: Int,
-        followingsCount: Int,
-        likesCount: Int,
-        likesReceivedCount: Int,
-        projectsCount: Int,
-        reboundsReceivedCount: Int,
-        teamsCount: Int,
-        canUploadShot: Bool,
-        type: String,
-        pro: Bool,
-        bucketsURL: URL,
-        followersURL: URL,
-        followingURL: URL,
-        likeURL: URL,
-        shotsURL: URL,
-        teamsURL: URL,
+        user: User,
         createdAt: Date,
         updatedAt: Date
     ) throws
@@ -61,24 +45,7 @@ extension GetBucketData {
             name: e.value("name"),
             description: e.value("description"),
             shotsCount: e.value("shots_count"),
-            bucketsCount: e.value("buckets_count"),
-            commentsRecievedCount: e.value("comments_received_count"),
-            followersCount: e.value("followers_count"),
-            followingsCount: e.value("followings_count"),
-            likesCount: e.value("likes_count"),
-            likesReceivedCount: e.value("likes_received_count"),
-            projectsCount: e.value("projects_count"),
-            reboundsReceivedCount: e.value("rebounds_received_count"),
-            teamsCount: e.value("teams_count"),
-            canUploadShot: e.value("can_upload_shot"),
-            type: e.value("type"),
-            pro: e.value("pro"),
-            bucketsURL: e.value("buckets_url", Transformer.url),
-            followersURL: e.value("followers_url", Transformer.url),
-            followingURL: e.value("following_url", Transformer.url),
-            likeURL: e.value("likes_url", Transformer.url),
-            shotsURL: e.value("shots_url", Transformer.url),
-            teamsURL: e.value("teams_url", Transformer.url),
+            user: e.value("user"),
             createdAt: e.value("created_at", Transformer.date),
             updatedAt: e.value("updated_at", Transformer.date))
     }
--- DribbbleKit/Sources/Transformer.swift
+++ DribbbleKit/Sources/Transformer.swift
@@ -16,7 +16,7 @@ struct Transformer {
         return { keyPath in
             Himotoki.Transformer {
                 guard let date = formatter.date(from: $0) else {
-                    throw DecodeError.typeMismatch(expected: "Date", actual: $0, keyPath: keyPath)
+                    throw typeMismatch(expected: Date.self, actual: $0, keyPath: keyPath)
                 }
                 return date
             }
@@ -25,10 +25,16 @@ struct Transformer {
     static let url: (KeyPath) -> Himotoki.Transformer<String, URL> = { keyPath in
         Himotoki.Transformer {
             guard let url = URL(string: $0) else {
-                throw DecodeError.typeMismatch(expected: "URL", actual: $0, keyPath: keyPath)
+                throw typeMismatch(expected: URL.self, actual: $0, keyPath: keyPath)
             }
             return url
         }
+    }
+
+    private static func typeMismatch(expected: Any.Type, actual: Any, keyPath: KeyPath) -> Error {
+        return DecodeError.typeMismatch(expected: String(describing: expected),
+                                        actual: String(describing: actual),
+                                        keyPath: keyPath)
     }
 }
 
@@ -37,9 +43,13 @@ extension Extractor {
         return try transfomer(keyPath).apply(value(keyPath))
     }
 
-    func valueOptional<T: Decodable, R>(_ keyPath: KeyPath, transfomer: (KeyPath) -> Himotoki.Transformer<T, R>) throws -> R? {
+    func valueOptional<T: Decodable, R>(_ keyPath: KeyPath, _ transfomer: (KeyPath) -> Himotoki.Transformer<T, R>) throws -> R? {
         return try valueOptional(keyPath).map { value in
             try transfomer(keyPath).apply(value)
         }
+    }
+
+    func dictionary<T: Decodable, R>(_ keyPath: KeyPath, _ transfomer: (KeyPath) -> Himotoki.Transformer<T, R>) throws -> [String: R] {
+        return try transfomer(keyPath).apply(dictionary(keyPath))
     }
 }
