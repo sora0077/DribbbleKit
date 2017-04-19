d4f95f98feb3c20c9400e15e901894ec5a00aadb
sora0077 <t.hayashi0077+github@gmail.com>
2017-04-19 17:28:51 +0900

    Add Create and Update shot request

---
+++ DribbbleKit/Sources/Shots/UpdateShot.swift
@@ -0,0 +1,32 @@
+//
+//  UpdateShot.swift
+//  DribbbleKit
+//
+//  Created by 林 達也 on 2017/04/19.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+import Foundation
+import APIKit
+import Himotoki
+
+public struct UpdateShot<Data: ShotData>: PostRequest {
+    public typealias Response = DribbbleKit.Response<Data>
+
+    public var path: String { return "/shots/\(id.value)" }
+
+    private let id: Shot.Identifier
+    public var title: String?
+    public var description: String?
+    public var tags: [String]?
+    public var teamId: Int?
+    public var lowProfile: Bool?
+
+    public init(id: Shot.Identifier) {
+        self.id = id
+    }
+
+    public func response(from object: Any, urlResponse: HTTPURLResponse) throws -> Response {
+        return try Response(meta: Meta(urlResponse: urlResponse), data: decodeValue(object))
+    }
+}
