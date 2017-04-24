80c52ebf8a27cae31eee112603192282dd0a8f09
t_hayashi <t.hayashi0077+github@gmail.com>
2017-04-24 21:39:20 +0900

    定義を分割

---
+++ DribbbleKit/Sources/Response.swift
@@ -0,0 +1,46 @@
+//
+//  Response.swift
+//  DribbbleKit
+//
+//  Created by 林達也 on 2017/04/24.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+import Foundation
+import APIKit
+import Alter
+
+public struct Response<Data> {
+    public let meta: Meta
+    public let data: Data
+}
+
+public final class Meta {
+    public struct RateLimit {
+        var limit: Int
+        var remaining: Int
+        var reset: Int
+    }
+    private let urlResponse: HTTPURLResponse
+    private var headers: [AnyHashable: Any] { return urlResponse.allHeaderFields }
+
+    public private(set) lazy var rateLimit: RateLimit = {
+        RateLimit(limit: self.headers[intValueForKey: "X-RateLimit-Limit"],
+                  remaining: self.headers[intValueForKey: "X-RateLimit-Remaining"],
+                  reset: self.headers[intValueForKey: "X-RateLimit-Reset"])
+    }()
+
+    subscript (_ key: AnyHashable) -> Any? {
+        return urlResponse.allHeaderFields[key]
+    }
+
+    init(urlResponse: HTTPURLResponse) {
+        self.urlResponse = urlResponse
+    }
+}
+
+private extension Dictionary where Key == AnyHashable, Value == Any {
+    subscript (intValueForKey key: Key) -> Int {
+        return  self[key] as? Int ?? 0
+    }
+}
