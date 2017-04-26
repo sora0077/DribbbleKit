c184ebdfd8a5ade2306f7c96ef3c1bc3c1e1c38e
sora0077 <t.hayashi0077+github@gmail.com>
2017-04-26 16:45:24 +0900

    Add create/delete following operation

---
+++ DribbbleKit/Sources/Users/Followers/UnfollowUser.swift
@@ -0,0 +1,22 @@
+//
+//  UnfollowUser.swift
+//  DribbbleKit
+//
+//  Created by 林 達也 on 2017/04/26.
+//  Copyright © 2017年 jp.sora0077. All rights reserved.
+//
+
+import Foundation
+import APIKit
+import Alter
+
+public struct UnfollowUser: DeleteRequest {
+    public typealias Response = DribbbleKit.Response<Void>
+
+    public var path: String { return "/users/\(target)/follow" }
+    private let target: String
+
+    public init(target: String) {
+        self.target = target
+    }
+}
