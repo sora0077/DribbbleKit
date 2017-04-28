9ba4d4d868bc12249666f9962547949a0bfff77d
sora0077 <t.hayashi0077+github@gmail.com>
2017-04-28 16:41:12 +0900

    ListUserLikes リクエストの追加

--- DribbbleKitTests/DataSet.swift
+++ DribbbleKitTests/DataSet.swift
@@ -75,6 +75,40 @@ struct DataSet {
         }
     }
 
+    struct TeamEntity: TeamData {
+        init(id: Int,
+             name: String,
+             username: String,
+             htmlURL: URL,
+             avatarURL: URL,
+             bio: String,
+             location: String,
+             links: [String : URL],
+             bucketsCount: Int,
+             commentsRecievedCount: Int,
+             followersCount: Int,
+             followingsCount: Int,
+             likesCount: Int,
+             likesReceivedCount: Int,
+             projectsCount: Int,
+             reboundsReceivedCount: Int,
+             shotsCount: Int,
+             teamsCount: Int,
+             canUploadShot: Bool,
+             type: String,
+             pro: Bool,
+             bucketsURL: URL,
+             followersURL: URL,
+             followingURL: URL,
+             likeURL: URL,
+             shotsURL: URL,
+             teamsURL: URL,
+             createdAt: Date,
+             updatedAt: Date
+            ) throws {
+        }
+    }
+
     struct BucketEntity: BucketData {
         init(id: Bucket.Identifier,
              name: String,
